2ec053222e355d2a6b38d2061003c6b270d51404
Ray <ray258063@gmail.com>
2025-03-31 21:33:39 +0800

    20250331-add Comment5

--- src/main/java/com/example/course5_9/config/KafkaConfig.java
+++ src/main/java/com/example/course5_9/config/KafkaConfig.java
@@ -23,6 +23,7 @@ import java.util.Map;
 public class KafkaConfig {
     //this is config
     //this is config
+    //this is config
 
     public static final String TEST_TOPIC = "test";
     public static final String JSON_TOPIC = "json";
